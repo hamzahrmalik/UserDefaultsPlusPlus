500210e5b4374438ea03c9770c372587d8c785b8
hamzahrmalik <hamzahrmalik@yahoo.co.uk>
2020-11-22 13:08:55 +0000

    Add SPM support

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "UserDefaults++",
+	platforms: [
+		.iOS(.v8)
+	],
+    products: [
+        .library(
+            name: "UserDefaultsPlusPlus",
+            targets: ["UserDefaultsPlusPlus"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "UserDefaultsPlusPlus"
+			path: "UserDefaultsPlusPlus"
+        )
+    ]
+)
